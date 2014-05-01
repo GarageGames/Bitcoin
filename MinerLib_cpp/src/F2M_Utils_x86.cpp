aa99dd033909d4587d1a7267128a1ddb5c336529
RonOhara-GG <rono@garagegames.com>
2014-04-30 18:27:51 -0700

    cpuid for clang

--- MinerLib_cpp/src/F2M_Utils_x86.cpp
+++ MinerLib_cpp/src/F2M_Utils_x86.cpp
@@ -1,13 +1,21 @@
 #include "F2M_Platform.h"
 #include "F2M_Utils.h"
 
+#ifdef WIN32
 #include <intrin.h> // __cpuid
+#else
+#include <cpuid.h>
+#endif
 
 
 bool F2M_HardwareSupportsSIMD()
 {
-    int CPUInfo[4];
+    unsigned int CPUInfo[4];
+#ifdef WIN32
         __cpuid(CPUInfo, 1);
+#else
+        __get_cpuid(1, &CPUInfo[0], &CPUInfo[1], &CPUInfo[2], &CPUInfo[3]);
+#endif
     bool sse = ((CPUInfo[3] & (1 << 26)) != 0);
     return sse;
 }
